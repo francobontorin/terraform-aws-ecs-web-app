7552c54431472394b734124023e9620133ab92ce
Franco Bontorin <bontorin@amazon.com>
2020-02-04 13:29:16 -0500

    Fixing Scaling Policies

--- main.tf
+++ main.tf
@@ -157,6 +157,7 @@ module "ecs_alb_service_task" {
   tags                              = var.tags
   volumes                           = var.volumes
   ecs_load_balancers                = local.load_balancers
+  ignore_changes_task_definition    = false
 }
 
 module "ecs_codepipeline" {
